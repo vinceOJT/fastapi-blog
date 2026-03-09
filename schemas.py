2d97a67eb96ca31928626322bcb621d6933158a1
vinceojt <jaca.ojt@gmail.com>
2026-03-10 04:54:33 +0800

    fixing an error 500 instead of max len i use max num in schema

--- schemas.py
+++ schemas.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, ConfigDict, Field
 # This schema code is the requirements for a post to be validated
 
 class PostBase(BaseModel):
-    title: str = Field(min_length=1, max_digits=100)
+    title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_digits=50)
+    author: str = Field(min_length=1, max_length=50)
 
 
 # This class requires the code at the top because it'll access it's parameters when creating
