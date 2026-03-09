72fca137c7b10345322b3961ada9a13fb84616bf
vinceojt <jaca.ojt@gmail.com>
2026-03-09 23:08:49 +0800

    changed context and name to author and content

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [{
     'id':1,
-    'name': 'john hickles',
+    'author': 'john hickles',
     'weight': 69.9,
     'title': 'about me',
-    'context': 'I live in 300 johnson st, at maywood pine groves',
+    'content': 'I live in 300 johnson st, at maywood pine groves',
     'date_posted': '03/04/2023'
 },
 {
     'id':2,
-    'name': 'mary hickles',
+    'author': 'mary hickles',
     'weight': 64.9,
     'title': 'about me too',
-    'context': 'I live in 200 johnson st, at maywood pine groves',
+    'content': 'I live in 200 johnson st, at maywood pine groves',
     'date_posted': '03/04/2023'
 
 }
@@ -34,20 +34,24 @@ posts: list[dict] = [{
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html",{"posts": posts, "title":"N"})
 
-@app.get("/api/posts")
-def get_posts():
-    return posts
-
 
 # creating a post retreiver base on id
-@app.get("/api/posts/{post_id}")
+@app.get("/posts/{post_id}")
 def get_posts(post_id: int, request: Request):
     for post in posts:
         if post.get("id") == post_id:
-            return post
-        else:
-            raise HTTPException(status_code=404, detail=f"Post: '{post_id}' not found")
+            post_title = post['title'][:50] # get the first 50 characters of the title 
+
+            return templates.TemplateResponse(request, "post.html",{"posts": posts, "title":post_title})
+    raise HTTPException(status_code=404, detail=f"Post: '{post_id}' not found")
     # return templates.TemplateResponse(request, "error.html")
+
+
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
 
 
 
