bb37236331d27b602d24ad58528491e889da0e6b
vinceojt <jaca.ojt@gmail.com>
2026-03-10 23:53:38 +0800

    added user creation classes in shcema.py

--- schemas.py
+++ schemas.py
@@ -1,7 +1,32 @@
-from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, Field, EmailStr 
+
+
+# Creating a user with required information
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+# Create user
+class UserCreate(UserBase):
+    pass
+
+# Response when create user
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id:int
+    image_file: str | None
+    image_pat: str 
+
+
+
+
+
+
 
 # This schema code is the requirements for a post to be validated
-
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
