e366f3f2141a4a4f2b5692f14297be04a6877609
vinceojt <jaca.ojt@gmail.com>
2026-03-10 23:39:25 +0800

    added comments to models.py

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from __future__ import annotations
+from __future__ import annotations # lower then ver 3.14
+# For older version of python this is use to annotate forward reference
+# Below we reference Post bfore it's even created, this is called forward reference
+
 
 from datetime import UTC, datetime
 
@@ -9,7 +12,7 @@ from database import Base
 
 
 class User(Base):
-    # Creating database table
+    # Creating database table with required parameters
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
@@ -21,8 +24,14 @@ class User(Base):
         default=None,
     )
 
+    # This is a relationship that creates a one to many relationship
+    # So one use can have many posts
     posts: Mapped[list[Post]] = relationship(back_populates="author")
+    # back_populates, gets the posts of the user then only shows their posts no one else's
 
+
+
+    # Checks if user uploads an image, if so return that image if not return a default image
     @property
     def image_path(self) -> str:
         if self.image_file:
@@ -30,20 +39,35 @@ class User(Base):
         return "/static/profile_pics/default.jpg"
 
 
+
+
+
+
+
+
 class Post(Base):
+    # Creating table for posts
+
+
     __tablename__ = "posts"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
     user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
+        ForeignKey("users.id"), # ref to the users table 
         nullable=False,
         index=True,
+        # Without the index parameter, it manually searches the user.id in the foreign table
+
+ 
     )
+    # datetime creation
     date_posted: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.now(UTC),
     )
 
+    # This is many post one user
     author: Mapped[User] = relationship(back_populates="posts")
+    
