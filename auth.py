1ee23b1a95075c2486464433741fb173b7a08bc5
vinceojt <jaca.ojt@gmail.com>
2026-03-19 00:04:22 +0800

    added hashing password and checker

--- auth.py
+++ auth.py
@@ -4,10 +4,20 @@ from datetime import UTC, datetime, timedelta
 
 import jwt
 from fastapi.security import OAuth2PasswordBearer
-from pwdlib import passswordHash
+from pwdlib import PasswordHash
 
 from config import Settings
 
+
+password_hash = PasswordHash.recommmended()
+oauth2_scheme = OAuth2PasswordBearer(tokenURl="api/users/token")
+
+
+def hash_password(password:str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password:str, hashed_password:str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
 
 
 
