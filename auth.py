7695d0005c579d74f17381d2a86810ead494be37
vinceojt <jaca.ojt@gmail.com>
2026-03-19 00:00:07 +0800

    updated important files and added auth.py with depencies

---
+++ auth.py
@@ -0,0 +1,20 @@
+
+from datetime import UTC, datetime, timedelta
+
+
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import passswordHash
+
+from config import Settings
+
+
+
+
+
+
+
+
+
+
+
