d6931e281bf60ecf994f1c4c8110208ebfc35725
vinceojt <jaca.ojt@gmail.com>
2026-03-23 22:24:02 +0800

    added get_current_user function and a type alias

--- auth.py
+++ auth.py
@@ -4,9 +4,19 @@ from datetime import UTC, datetime, timedelta
 
 import jwt
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 from pwdlib import PasswordHash
 
 from config import settings
+
+from typing import Annotated
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+import models
+from database import get_db
 
 
 password_hash = PasswordHash.recommended()
@@ -52,6 +62,58 @@ def verify_access_token(token: str) -> str | None:
         return None
     else:
         return payload.get("sub")
+
+
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Annotated[AsyncSession, Depends(get_db)],) -> models.User:
+    user_id = verify_access_token(token)
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    try:
+        user_id_int = int(user_id)
+    except (TypeError, ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    result = await db.execute(
+        select(models.User).where(models.User.id == user_id_int),
+    )
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+# models.User gets a user object, Depends(function) is meta data about that user
+CurrentUser = Annotated[models.User, Depends(get_current_user)]
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
