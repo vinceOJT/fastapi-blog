675c2977dfa755748c65b85adc5f88cd9c86f3a5
vinceojt <jaca.ojt@gmail.com>
2026-03-23 23:08:07 +0800

    updated post.html layout and added code on main

--- main.py
+++ main.py
@@ -184,6 +184,15 @@ async def register_page(request: Request):
 
 
 
+@app.get("/account", include_in_schema=False)
+async def account_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
+
+
 
 
 
